43703e9379246cbc631ce544af27905d04108b5d
Yuleni1 <myuleni4@gmail.com>
2022-07-09 12:17:55 -0400

    added employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require('../lib/Employee');
+
+test('creates a employee object', ()=>{
+    const employee = new Employee('Yuleni', '650','yuleni@gmail.com' )
+
+expect(employee.name).toBe('Yuleni');
+expect(employee.id).toBe('650');
+expect(employee.email).toBe('yuleni@gmail.com');
+});
+
