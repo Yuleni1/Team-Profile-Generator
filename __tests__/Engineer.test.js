9e74abedd367a587218983b19ca94a1f33fc48df
Yuleni1 <myuleni4@gmail.com>
2022-07-09 13:58:49 -0400

    added Engineer test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,16 @@
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+test('creates github', ()=>{
+    const engineer = new Engineer('Yuleni', '650','yuleni@gmail.com' ,'Yuleni1');
+
+    expect(engineer.gitHub).toBe('Yuleni1');
+
+    })
+
+test('gets github', ()=>{
+    const engineer = new Engineer('Yuleni', '650','yuleni@gmail.com' ,'Yuleni1');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining('Yuleni1'));
+    //expect(engineer.getGithub()).toBe('Yuleni1');
+})
