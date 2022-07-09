588599129ff026f190d4d798ecbb7626bb684532
Yuleni1 <myuleni4@gmail.com>
2022-07-09 14:15:10 -0400

    added test for Intern

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,17 @@
+const Employee = require('../lib/Employee');
+const Intern = require('../lib/Intern');
+
+test('creates school', ()=>{
+    const intern = new Intern('Yuleni', '650','yuleni@gmail.com' ,'university');
+
+    expect(intern.school).toBe('university');
+
+    })
+
+    test('gets school', ()=>{
+        const intern = new Intern('Yuleni', '650','yuleni@gmail.com' ,'university');
+    
+        expect(intern.getSchool()).toEqual(expect.stringContaining('university'));
+        //expect(engineer.getGithub()).toBe('Yuleni1');
+    })
+    
