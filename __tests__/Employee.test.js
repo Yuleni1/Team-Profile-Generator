814c356e3508c5508444d5688aad6478ada6d2ef
Yuleni1 <myuleni4@gmail.com>
2022-07-09 13:22:57 -0400

    added tests to employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,5 +8,27 @@ expect(employee.id).toBe('650');
 expect(employee.email).toBe('yuleni@gmail.com');
 });
 
+test("gets employee name",() =>{
+    const employee = new Employee('Yuleni')
 
+    expect(employee.getName()).toBe('Yuleni')
+})
+
+test("gets employee id",()=>{
+    const employee = new Employee('Yuleni','650')
+
+    
+    expect(employee.getId()).toBe('650')
+})
+test("gets employee id",()=>{
+    const employee = new Employee('650','yuleni@gmail.com')
+
+    
+    expect(employee.getId()).toBe('yuleni@gmail.com')
+})
+
+test("gets role",()=>{
+    const employee = new Employee('Yuleni')
+    expect(employee.getRole()).toBe('Employee')
+})
 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,7 +2,7 @@ const Employee = require('../lib/Employee');
 const Manager = require('../lib/Manager');
 
 test('creates office number', ()=>{
-    const manager = new Manager('12b');
+    const manager = new Manager('Yuleni', '650','yuleni@gmail.com' ,'12b');
 
     expect(manager.officeNumber).toBe('12b');
 
